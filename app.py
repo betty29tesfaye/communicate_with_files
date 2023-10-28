02b1f5ec4a460ff6fcaf24d6ff97dbe501e37afe
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2023-10-28 17:07:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import Helper
 uploaded_file = st.file_uploader('Upload a file')
 if uploaded_file is not None:
-  st.write("file uploaded")
+   save_uploadedfile(uploaded_file )
+   raw_pdf_document_content = get_file_content(str(uploaded_file.name))
+   pdf_document_chunks = text_splitter.split_text(raw_pdf_document_content)
+   doc_search_paper = create_vector_index(pdf_document_chunks)
+   print(doc_search_paper)
+   question_from_user = st.text_input("I am your file, please enter your question, I can answer")
+   if question_from_user is not None:
+    results = speak_with_file(str(uploaded_file.name),question_from_user)
+    answer = results["answer"]
+    confidence_score = results["score"]
+    st.write(f"{answer} \n {confidence_score}")
