74b3fda111a5031e3af8567bc07b3b4b662d4af9
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2023-10-28 18:13:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 import streamlit as st
 import helper as helper1
-#Helper.py
+
+
+
+def save_uploadedfile(uploadedfile):
+     with open(uploadedfile.name,"wb") as f:
+         f.write(uploadedfile.getbuffer())
+     return st.success("Saved")
+  
+def get_file_content(file_path):
+  if("pdf" in file_path):
+   loader=UnstructuredFileLoader(file_path)
+  elif("png" in file_path):
+   loader = UnstructuredImageLoader(file_path)
+  else:
+    return null
+  document = loader.load()
+  document_content = '\n'.join(doc.page_content for doc in document)
+  return document_content
+text_splitter = CharacterTextSplitter(
+    separator = "\n\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200,
+    length_function = len,
+)
+os.environ['OPENAI_API_KEY']= "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"
+#"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" #previous key
+embeddings = OpenAIEmbeddings()
+
+
 uploaded_file = st.file_uploader('Upload a file')
 if uploaded_file is not None:
    save_uploadedfile(uploaded_file )
