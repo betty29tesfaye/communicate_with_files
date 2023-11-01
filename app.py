a1b4408b7b6046a37b2603a9c5bfccb0b244f6fa
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2023-11-01 18:09:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,9 @@
-import warnings
-warnings.filterwarnings('ignore')
-import os
-import streamlit as st
-from langchain.document_loaders.image import UnstructuredImageLoader
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
+import communicate_with_files/helper.py
 
-
-def save_uploadedfile(uploadedfile):
-     with open(uploadedfile.name,"wb") as f:
-         f.write(uploadedfile.getbuffer())
-     return st.success("Saved")
-  
-def get_file_content(file_path):
-  if("pdf" in file_path):
-   loader=UnstructuredFileLoader(file_path)
-  elif("png" in file_path):
-   loader = UnstructuredImageLoader(file_path)
-  else:
-    return null
-  document = loader.load()
-  document_content = '\n'.join(doc.page_content for doc in document)
-  return document_content
-text_splitter = CharacterTextSplitter(
-    separator = "\n\n",
-    chunk_size = 1000,
-    chunk_overlap  = 200,
-    length_function = len,
-)
 # os.environ['OPENAI_API_KEY']= "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"
 #"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"
 #"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" #previous key
-embeddings = OpenAIEmbeddings()
 
-def create_vector_index(text_chunks):
-   return FAISS.from_texts(text_chunks, embeddings)
-     
-chain = load_qa_chain(OpenAI(),chain_type = "map_rerank",return_intermediate_steps=True)
-
-def speak_with_file(file_path,query):
-  file_content = get_file_content(file_path)
-  file_splitter = text_splitter.split_text(file_content)
-  doc_search = create_vector_index(file_splitter)
-  documents = doc_search.similarity_search(query)
-  results = chain({"input_documents":documents,"question":query},return_only_outputs=True)
-  results = results['intermediate_steps'][0]
-  return results
-     
 uploaded_file = st.file_uploader('Upload a file')
 if uploaded_file is not None:
    save_uploadedfile(uploaded_file )
